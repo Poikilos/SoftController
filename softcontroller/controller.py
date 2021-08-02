1f66cefa08d5f63fc3f7298c97c2584ec127ec7c
poikilos <7557867+poikilos@users.noreply.github.com>
2021-08-02 03:47:57 -0400

    Set the state when added and check for a valid sid to prevent crashes. Allow lt&gt comparisons in getBool.

--- softcontroller/controller.py
+++ softcontroller/controller.py
@@ -1,7 +1,17 @@
 #!/usr/bin/env python
+'''
+This file is part of the SoftController project
+Copyright 2021 Jake "Poikilos" Gustafson
 
-_verbose_controller_stats = True
+License: MIT License (See <https://github.com/poikilos/SoftController>)
+'''
+import sys
+
+_verbose_controller_stats = False
 _show_controller_stats = False
+
+def error(msg):
+    sys.stderr.write(str(msg)+"\n")
 
 def set_controller_stats(on):
     '''
@@ -16,10 +26,15 @@ def set_controller_stats(on):
 class Controller:
     '''
     Each button, axis, hat, or key is mapped to a single sid (software
-    id) identifying a virtual actuator. In the case of a key, it can
-    add a specific value to the sid. Each sid is a string that is a key
-    in the self._states dict. The value is any number, such as 0 for
-    not pressed, 1 for pressed, or -1 for the opposite direction.
+    id) identifying an action in your own terms. In that way, you name
+    a virtual actuator and map a real actuator (key(s), button, axis,
+    hat (can affect two sids)) to it.
+
+    A keyboard keycode can set a virtual actuator to a value specified
+    on add if set to True (or 0 if set to False). Each sid is a string
+    that is a key in the self._states dict. The value is any number,
+    such as 0 for not pressed, 1 for pressed, or -1 for the opposite
+    direction.
 
     The state of each virtual actuator can be affected by multiple
     types of hardware actuators and up to two keyboard keys. If 4 keys
@@ -52,6 +67,7 @@ class Controller:
 
         self._hat_to_sids = {}  # hat [hatID] values to sids: (sid, sid)
         self._sid_to_hat = {}  # reverse map: either axis sid to hatID
+        self._inversions = {}  # hat [hatID]: if value is True invert y
 
 
     def addKeyAsAxisValue(self, keycode, sid, value):
@@ -66,8 +82,11 @@ class Controller:
         except AttributeError:
             raise TypeError("The sid must be some kid of string")
         self._kc_to_sid[str(keycode)] = sid
-        self._sid_to_kcs[sid] = keycode
+        if self._sid_to_kcs.get(sid) is None:
+            self._sid_to_kcs[sid] = []
+        self._sid_to_kcs[sid].append(keycode)
         self._kc_value[str(keycode)] = value
+        self._states[sid] = 0
 
     def addAxis(self, axisIndex, sid):
         '''
@@ -81,6 +100,7 @@ class Controller:
             raise TypeError("The sid must be some kid of string")
         self._ax_to_sid[str(axisIndex)] = sid
         self._sid_to_ax[sid] = axisIndex
+        self._states[sid] = 0
 
     def addButton(self, button, sid):
         '''
@@ -94,9 +114,14 @@ class Controller:
             raise TypeError("The sid must be some kid of string")
         self._btn_to_sid[str(button)] = sid
         self._sid_to_btn[sid] = button
+        self._states[sid] = 0
 
     def addKey(self, keycode, sid):
         '''
+        This only sets a boolean value. For arrow keys: instead of
+        calling this method, call addKeyAsAxisValue twice with the
+        same sid but a different keycode and value (such as -1 and 1).
+
         Sequential arguments:
         keycode -- a hardware keycode
         sid -- Choose a new/existing virtual actuator.
@@ -105,6 +130,7 @@ class Controller:
         if self._sid_to_kcs.get(sid) is None:
             self._sid_to_kcs[sid] = []
         self._sid_to_kcs[sid].append(keycode)
+        self._states[sid] = 0
 
     # endregion initialization
 
@@ -115,13 +141,51 @@ class Controller:
         Get the value as True if > 0, otherwise False.
 
         Sequential arguments:
-        sid -- Choose an existing virtual actuator.
+        sid -- Choose an existing virtual actuator. If it contains '>'
+               or '<' then the comparison operator will be used:
+               - You must place the sid on the left side of the operator
+                 if you use one.
         '''
-        got = self._states.get(sid)
+        this_show_stats = _show_controller_stats
+        if _verbose_controller_stats:
+            this_show_stats = True
+
+        got = None
+        result = None
+        old = None
+        if '<' in sid:
+            old = sid
+            sid, r_operand = sid.split('<')
+            got = self._states.get(sid)
+            if got is None:
+                raise KeyError("{} is not a registered controller sid"
+                               "(operation: {}). _states: {}"
+                               "".format(sid, old, self._states))
+            result = int(got < float(r_operand))
+        elif '>' in sid:
+            old = sid
+            sid, r_operand = sid.split('>')
+            got = self._states.get(sid)
+            if got is None:
+                raise KeyError("{} is not a registered controller sid"
+                               "(operation: {}). _states: {}"
+                               "".format(sid, old, self._states))
+            result = int(got > float(r_operand))
+
+        else:
+            got = self._states.get(sid)
+            result = got
+        if this_show_stats:
+            if old is not None:
+                # print("{} was sid"
+                #       "(operation: {}). result: {}"
+                #       "".format(sid, old, (result > 0)))
+                pass
+
         if got is None:
             raise KeyError("{} is not a registered controller sid"
-                           "".format(sid))
-        return got > 0
+                           ". _states: {}".format(sid, self._states))
+        return result > 0
 
     def getRaw(self, sid):
         '''
@@ -132,6 +196,8 @@ class Controller:
         if got is None:
             raise KeyError("{} is not a registered controller sid"
                            "".format(sid))
+        # NOTE: Use _inversions on set, not here on get, since multiple
+        #       physical actuators can affect a virtual one.
         return got
 
     def getInt(self, sid):
@@ -142,20 +208,20 @@ class Controller:
         Sequential arguments:
         sid -- Choose an existing virtual actuator.
         '''
+        this_show_stats = _show_controller_stats
+        if _verbose_controller_stats:
+            this_show_stats = True
         got = self._states.get(sid)
         if got is None:
             raise KeyError("{} is not a registered controller sid"
                            "".format(sid))
+        # NOTE: Use _inversions on set, not here on get, since multiple
+        #       physical actuators can affect a virtual one.
         if abs(got) > self.deadZone:
             if got > 0:
                 return 1
             else:
                 return -1
-        else:
-            if _show_controller_stats:
-                print("sid {}: abs({})"
-                      " didn't get outside of self.deadZone"
-                      "".format(sid, got))
         return 0
 
     def toKeys(self):
@@ -174,7 +240,11 @@ class Controller:
             keyValue = self._kc_value.get(str(kc))
             if keyValue is None:
                 keyValue = 1
-            stateValue = self._states[self._kc_to_sid[str(kc)]]
+            sid = self._kc_to_sid.get(str(kc))
+            if sid is None:
+                pressed.append(False)
+                continue
+            stateValue = self._states[sid]
             if abs(stateValue) > self.deadZone:
                 # Determine which key affected the the sid
                 # since the key may set the sid to a certain
@@ -203,7 +273,12 @@ class Controller:
             value = self._kc_value.get(str(keycode))
             if value is None:
                 value = int(on)
-        self._states[self._kc_to_sid[str(keycode)]] = value
+        sid = self._kc_to_sid.get(str(keycode))
+        if sid is None:
+            error("Warning: sid is None for keycode {}".format(keycode))
+            return False
+        self._states[sid] = value
+        return True
 
     def setAxis(self, axis, value):
         '''
@@ -216,7 +291,17 @@ class Controller:
         value -- The raw value is usually -1.0 to 1.0 but it doesn't
                  matter. You can get it later with any "get" method
                  that gets a single value.
+
+        returns:
+        True if the axis is mapped, otherwise False (The axis will not
+        set anything at all if it is not mapped to some sid)
         '''
+        this_show_stats = _show_controller_stats
+        if _verbose_controller_stats:
+            this_show_stats = True
+        sid = self._ax_to_sid.get(str(axis))
+        if sid is None:
+            return False
         if abs(value) > self.deadZone:
             try:
                 tmp = float(value)
@@ -224,10 +309,13 @@ class Controller:
                 print("The value for axis {} should be a number"
                       " but is {}."
                       "".format(axis, value))
-            self._states[self._ax_to_sid[str(axis)]] = value
+            self._states[sid] = value
             if this_show_stats:
                 print("joystick axis {} = {}"
                       "".format(axis, value))
+        else:
+            self._states[sid] = 0
+        return True
 
     def setButton(self, button, on):
         '''
@@ -239,9 +327,23 @@ class Controller:
         on -- True for pressed, False for released (converted to 0 or 1
               if True or False, otherwise the integer is preserved but
               other values are not supported)
+
+        Returns:
+        True if mapped, otherwise False (The button won't set
+        anything if it isn't mapped)
         '''
-        sid = self._btn_to_sid[str(button)]
+        this_show_stats = _show_controller_stats
+        if _verbose_controller_stats:
+            this_show_stats = True
+
+        sid = self._btn_to_sid.get(str(button))
+        if this_show_stats:
+            print("set {} to {} via button {}"
+                  "".format(sid, on, button))
+        if sid is None:
+            return False
         self._states[sid] = int(on)
+        return True
 
     def setHat(self, hat, values):
         '''
@@ -254,21 +356,32 @@ class Controller:
         values -- The tuple containing the values of the hat, usually
                   two coordinates, each of which can be -1, 0, or 1.
                   Only a two-long tuple is supported.
+
+        True if mapped, otherwise False (The hat won't set
+        anything if it isn't mapped)
         '''
         if len(values) != 2:
             raise ValueError("The hat values argument must be a tuple"
                              " or list that has two elements.")
-        sid0, sid1 = self._hat_to_sids[str(hat)]
+        sids = self._hat_to_sids.get(str(hat))
+        if sids is None:
+            return False
+        sid0, sid1 = sids
         self._states[sid0] = values[0]
-        self._states[sid1] = values[1]
+        multiplier = 1
+        if self._inversions.get(sid1) is True:
+            multiplier = -1
+        self._states[sid1] = values[1] * multiplier
+        return True
 
-    def addHat(self, hat, sids):
+    def addHat(self, hat, sids, invert_y):
         '''
         Sequential arguments:
         hat -- Specify the hat index from the hardware controller.
         sids -- Specify a tuple containing two sids to affect since a
                 hat (such as a d-pad) has two values, usually
                 interpreted as x and y.
+        invert_y -- if True, multiply y value by -1
         '''
         if len(sids) != 2:
             raise ValueError("The hat sids argument must be a tuple"
@@ -277,6 +390,7 @@ class Controller:
         self._sid_to_hat[sids[0]] = hat
         self._sid_to_hat[sids[1]] = hat
         # ^ Both sids point to the same hat.
+        self._inversions[sids[1]] = invert_y
 
     def clearPressed(self):
         for sid in self._states.keys():
